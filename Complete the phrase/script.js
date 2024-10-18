a57e2a489b9bd6b2d82dcae8fed9c17792bc33e7
ismaeldorlus <i.dorlus@share.epsb.ca>
2024-10-18 15:21:26 -0600

    Add files via upload

---
+++ Complete the phrase/script.js
@@ -0,0 +1,28 @@
+// Complete the Phrase
+document.getElementById("btn1").addEventListener("click", btn1);
+function btn1() {
+  let idiom1 = "If there a will there's a way!";
+  alert(idiom1);
+}
+
+document.getElementById("btn2").addEventListener("click", btn2);
+function btn2() {
+  let idiom2 = "What doesn't kill you makes you stronger!";
+  alert(idiom2);
+}
+
+document.getElementById("btn3").addEventListener("click", btn3);
+function btn3() {
+  let idiom3 = "It's raining cats and dogs!";
+  alert(idiom3);
+}
+document.getElementById("btn4").addEventListener("click", btn4);
+function btn4() {
+  let idiom4 = "Sharpest tool in the shed!";
+  alert(idiom4);
+}
+document.getElementById("btn5").addEventListener("click", btn5);
+function btn5() {
+  let idiom5 = "No pain, no gain!";
+  alert(idiom5);
+}
